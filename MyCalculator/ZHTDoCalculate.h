988e8c0c59b8f33c19b395ad9182d12c30b960f9
Gh9063221 <zhengt.cn@gmail.com>
2013-04-10 06:34:24 +0800

    add 1 class: DoCaculate

---
+++ MyCalculator/ZHTDoCalculate.h
@@ -0,0 +1,13 @@
+//
+//  ZHTDoCalculate.h
+//  MyCalculator
+//
+//  Created by zht on 13-4-9.
+//  Copyright (c) 2013年 zht. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZHTDoCalculate : NSObject
+
+@end
---
+++ MyCalculator/ZHTDoCalculate.m
@@ -0,0 +1,13 @@
+//
+//  ZHTDoCalculate.m
+//  MyCalculator
+//
+//  Created by zht on 13-4-9.
+//  Copyright (c) 2013年 zht. All rights reserved.
+//
+
+#import "ZHTDoCalculate.h"
+
+@implementation ZHTDoCalculate
+
+@end
